faec6a70d1dc0418461108b43c568e5e942fc161
Viktoriia Fokina <viikkii.f@gmail.com>
2021-11-16 00:37:22 +0200

    ДЗ-09
    подготовка к выполнению ДЗ

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,14 +2,11 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const btn = document.querySelector('.change-color');
-const span = document.querySelector('.color');
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
 
-btn.addEventListener("click", (changeColor) => {
+btnStart.addEventListener("click", () => {
   const color = getRandomHexColor();
-  span.textContent = color;
   document.body.style.background = color;
  }
 )
-
-console.log(getRandomHexColor());
