0b8ae99e62cd753fdb7ea97309ad9189d048ed62
Viktoriia Fokina <viikkii.f@gmail.com>
2021-11-16 00:51:59 +0200

    ДЗ-09
    правки

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -4,9 +4,22 @@ function getRandomHexColor() {
 
 const btnStart = document.querySelector('button[data-start]');
 const btnStop = document.querySelector('button[data-stop]');
+let timerId = null;
 
 btnStart.addEventListener("click", (changeColor) => {
   const color = getRandomHexColor();
   document.body.style.background = color;
  }
 )
+
+getRandomHexColor()
+btnStart.addEventListener("click", () => {
+  timerId = setInterval(() => {
+
+  }, 10);
+});
+
+
+btnStop.addEventListener("click", () => {
+  clearInterval(timerId);
+});
