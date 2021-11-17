129e1a9bb5af64d96d2dbc9e4f20d5a6e2b47849
Viktoriia Fokina <viikkii.f@gmail.com>
2021-11-17 23:30:37 +0200

    ДЗ-09
    подготовка задачи 02

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,8 +1,12 @@
+import { time } from 'console';
 import flatpickr from 'flatpickr';
 // Дополнительный импорт стилей
 import 'flatpickr/dist/flatpickr.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { start } from 'repl';
 require("flatpickr/dist/themes/dark.css");
+
+
 
 const options = {
   enableTime: true,
@@ -14,22 +18,78 @@ const options = {
   },
 };
 
+// логика таймера и его остановка
+const timer = {
+    intervalId: null,
+    isActive: false,
 
+    start() {
+        if (this.isActive) {
+            return;
+        }
+        const startTime = Date.now();
+        this.isActive = true;
+
+        this.intervalId = setInterval(() => {
+            const currentTIme = Date.now();
+            const deltaTime = currentTIme - startTime;
+            const time = convertMs(deltaTime);
+            updateClockface(time);
+            console.log(time)
+        }, 1000)
+    }, 
+    stop() {
+        clearInterval(this.intervalId)
+        this.isActive = false;
+    }
+};
+
+// обращаемся к кнопке
+const refs = {
+    btnStart: document.querySelector('button[data-start]'),
+    inputForm: document.querySelector('#datetime-picker'),
+    daysData: document.querySelector('.timer [data-days]'),
+    hoursData: document.querySelector('.timer [data-hours]'),
+    minutesData: document.querySelector('.timer [data-minutes]'),
+    secondsData: document.querySelector('.timer [data-seconds]')
+};
+
+flatpickr(refs.inputForm, options)
+
+// вызываем запуск таймера + добавлем слушателя
+refs.btnStart.addEventListener('click', () => {
+    timer.start();
+    refs.btnStart.setAttribute("disabled", "true");
+})
+
+// выводим в интерфейс 
+function updateClockface(time) {
+    refs.daysData.textContent = addLeadingZero(time.days);
+    refs.hoursData.textContent = addLeadingZero(time.hours);
+    refs.minutesData.textContent = addLeadingZero(time.minutes);
+    refs.secondsData.textContent = addLeadingZero(time.seconds);
+}
+
+// добавляем 0 перед 0-9
+function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+}
+
+// функция рассчета секундомера
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
   // Remaining days
-  const days = Math.floor(ms / day);
+  const days = addLeadingZero(Math.floor(ms / day));
   // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
   // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
   // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
 
   return { days, hours, minutes, seconds };
 }
